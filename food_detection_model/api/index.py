fdf635f42074872a1ffa951822d14fd6f86881cb
Harini-112 <harinibaskaran3@gmail.com>
2025-08-26 13:44:48 +0530

    update

--- food_detection_model/api/index.py
+++ food_detection_model/api/index.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, jsonify, render_template_string
 from main import detect_food, classify_food, estimate_freshness, get_nutrition
 
+from app.app import app  # import your Flask app instance
+
+# Vercel looks for this "handler"
+def handler(request, response):
+    return app(request.environ, response.start_response)
+
 app = Flask(__name__)
 
 @app.route('/')
